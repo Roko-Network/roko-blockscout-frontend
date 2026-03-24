7aad43ad774a9f350a8d8f178c7190989c6c2096
Roko Network <dev@roko.network>
2026-03-24 19:43:19 +0100

    feat: replace compact stats with full temporal cards on homepage

--- src/slices/home/pages/index/Home.tsx
+++ src/slices/home/pages/index/Home.tsx
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: LicenseRef-Blockscout
 
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, Heading } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 import { HomeDataContextProvider } from 'src/slices/home/contexts/home-data-context';
@@ -8,10 +9,18 @@ import { HomeRpcDataContextProvider } from 'src/slices/home/contexts/rpc-data-co
 
 import AdBanner from 'src/features/ads/banner/components/AdBanner';
 import LatestArbitrumL2Batches from 'src/features/rollup/arbitrum/pages/home/LatestArbitrumL2Batches';
-import TemporalStatsHome from 'src/features/temporal/components/home/TemporalStatsHome';
+import {
+  fetchTemporalConsensusTime,
+  fetchTemporalQueueStats,
+  fetchTemporalWatermark,
+} from 'src/features/temporal/api/temporal-rpc';
+import TemporalConsensusCard from 'src/features/temporal/components/TemporalConsensusCard';
+import TemporalQueueCard from 'src/features/temporal/components/TemporalQueueCard';
+import TemporalWatermarkCard from 'src/features/temporal/components/TemporalWatermarkCard';
 
 import config from 'src/config';
 import useIsMobile from 'src/shared/hooks/useIsMobile';
+import { SECOND } from 'src/toolkit/utils/consts';
 
 import LatestBlocks from './blocks/LatestBlocks';
 import ChainIndicators from './charts/ChainIndicators';
@@ -22,8 +31,33 @@ import Transactions from './txs/Transactions';
 
 const rollupFeature = config.features.rollup;
 
+const BLOCK_TIME_MS = 6 * SECOND;
+
 const Home = () => {
   const isMobile = useIsMobile();
+
+  const watermarkQuery = useQuery({
+    queryKey: [ 'temporal_watermark' ],
+    queryFn: fetchTemporalWatermark,
+    refetchInterval: BLOCK_TIME_MS,
+  });
+
+  const consensusTimeQuery = useQuery({
+    queryKey: [ 'temporal_consensus_time' ],
+    queryFn: fetchTemporalConsensusTime,
+    refetchInterval: BLOCK_TIME_MS,
+  });
+
+  const queueStatsQuery = useQuery({
+    queryKey: [ 'temporal_queue_stats' ],
+    queryFn: fetchTemporalQueueStats,
+    refetchInterval: BLOCK_TIME_MS,
+  });
+
+  const temporalLoading =
+    watermarkQuery.isLoading ||
+    consensusTimeQuery.isLoading ||
+    queueStatsQuery.isLoading;
 
   const leftWidget = (() => {
     if (rollupFeature.isEnabled && !rollupFeature.homepage.showLatestBlocks) {
@@ -45,8 +79,27 @@ const Home = () => {
             <Stats/>
             <ChainIndicators/>
           </Flex>
-          <Box mt={ 2 }>
-            <TemporalStatsHome/>
+          <Box mt={ 6 }>
+            <Heading as="h2" fontSize="lg" fontFamily="heading" mb={ 3 }>
+              Temporal Ordering
+            </Heading>
+            <Grid
+              templateColumns={{ base: '1fr', lg: 'repeat(3, 1fr)' }}
+              gap={ 6 }
+            >
+              <TemporalWatermarkCard
+                data={ watermarkQuery.data }
+                isLoading={ temporalLoading }
+              />
+              <TemporalConsensusCard
+                data={ consensusTimeQuery.data }
+                isLoading={ temporalLoading }
+              />
+              <TemporalQueueCard
+                data={ queueStatsQuery.data }
+                isLoading={ temporalLoading }
+              />
+            </Grid>
           </Box>
           { !isMobile && config.slices.home.highlights && <Highlights mt={ 3 }/> }
           { isMobile && <AdBanner mt={ 6 } mx="auto" justifyContent="center" format="mobile"/> }
